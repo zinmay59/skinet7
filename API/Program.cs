861f39d9efd23f4a32445c5806038beef41b6933
zinmay <zinmay@mycondo.sg>
2023-12-08 00:29:01 +0630

    End of section 4

--- API/Program.cs
+++ API/Program.cs
@@ -15,6 +15,8 @@ builder.Services.AddDbContext<StoreContext>(opt =>
 });
 
 builder.Services.AddScoped<IProductRepository, ProductRepository>();
+builder.Services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 
 var app = builder.Build();
 
@@ -24,6 +26,9 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.UseStaticFiles();
+
 app.UseAuthorization();
 
 app.MapControllers();
